357cce998f778d6947e5dc72a75f56515a6da1f9
Brariv <119123309+Brariv@users.noreply.github.com>
2024-08-23 19:00:15 -0600

    Comments

--- proyecto.py
+++ proyecto.py
@@ -1,15 +1,18 @@
 
 def main():
     opt = 0
-    conjuntos = {"Universo": ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9"]}
+    conjuntos = {#Conjuntos Predefinidos y A;macen de Conjuntos
+        #Conjunto Universo
+        "Universo": ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9"]
+        }
 
-    while(opt != 3):
+    while(opt != 3): #Menu Principal
         print("Opciones: ")
         print("1. Construir Conjunto")
         print("2. Operar Conjunto")
         print("3. Salir")
 
-        while True:
+        while True: #Validar Opción
             opt = input("Ingrese una opción: ")
             try :
                 opt = int(opt)
@@ -19,16 +22,14 @@ def main():
                 continue
 
 
-        if opt == 1:
-            conjuntos.update(construir_conjunto(conjuntos["Universo"]))
+        if opt == 1: #Construir Conjunto
+            conjuntos.update(construir_conjunto(conjuntos))
             
             
             
-        elif opt == 2:
+        elif opt == 2:#Menu Secundario (Operar Conjunto)
             print("Operar Conjunto")
-            print ("Conjuntos Disponibles: ")
-            for key in conjuntos:
-                print(key)
+            
 
             print("Operaciones: ")
             print("1. Complemento")
@@ -38,7 +39,7 @@ def main():
             print("5. Diferencia Simétrica")
             print("6. Cancelar")
 
-            while True:
+            while True: #Validar Opción
                 operacion = input("Ingrese la operación: ")
                 try :
                     operacion = int(operacion)
@@ -46,65 +47,136 @@ def main():
                 except ValueError:
                     print("Opción no válida")
                     continue
+
+            print ("Conjuntos Disponibles: ")#Mostrar Conjuntos Disponibles
+            for key in conjuntos:
+                print(key)
             
             if operacion == 1:
                 print("Complemento")
-                conjunto = input("Ingrese el conjunto a operar: ")
+                while True: #Validar si el Conjunto Existe
+                    conjunto = input("Ingrese el conjunto a operar: ")
+                    if conjunto in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
                 print("Conjunto", conjunto, ":", conjuntos[conjunto])
-                print("Complemento:", complemento(conjuntos["Universo"], conjuntos[conjunto]))
+                print("Complemento:", complemento(conjuntos["Universo"], conjuntos[conjunto])) #Mostrar Complemento
+
             elif operacion == 2:
                 print("Unión")
-                conjunto1 = input("Ingrese el primer conjunto a operar: ")
-                conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                while True:#Validar si el Conjunto Existe
+                    conjunto1 = input("Ingrese el primer conjunto a operar: ")
+                    if conjunto1 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
+                while True:#Validar si el Conjunto Existe
+                    conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                    if conjunto2 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
                 print("Conjunto", conjunto1, ":", conjuntos[conjunto1])
                 print("Conjunto", conjunto2, ":", conjuntos[conjunto2])
-                print("Unión:", union(conjuntos[conjunto1], conjuntos[conjunto2]))
+                print("Unión:", union(conjuntos[conjunto1], conjuntos[conjunto2])) #Mostrar Unión
+
             elif operacion == 3:
                 print("Intersección")
-                conjunto1 = input("Ingrese el primer conjunto a operar: ")
-                conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                while True:#Validar si el Conjunto Existe
+                    conjunto1 = input("Ingrese el primer conjunto a operar: ")
+                    if conjunto1 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
+                while True:#Validar si el Conjunto Existe
+                    conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                    if conjunto2 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
                 print("Conjunto", conjunto1, ":", conjuntos[conjunto1])
                 print("Conjunto", conjunto2, ":", conjuntos[conjunto2])
-                print("Intersección:", interseccion(conjuntos[conjunto1], conjuntos[conjunto2]))
+                print("Intersección:", interseccion(conjuntos[conjunto1], conjuntos[conjunto2])) #Mostrar Intersección
+
             elif operacion == 4:
                 print("Diferencia")
-                conjunto1 = input("Ingrese el primer conjunto a operar: ")
-                conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                while True:#Validar si el Conjunto Existe
+                    conjunto1 = input("Ingrese el primer conjunto a operar: ")
+                    if conjunto1 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
+                while True:#Validar si el Conjunto Existe
+                    conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                    if conjunto2 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
                 print("Conjunto", conjunto1, ":", conjuntos[conjunto1])
                 print("Conjunto", conjunto2, ":", conjuntos[conjunto2])
-                print("Diferencia:", diferencia(conjuntos[conjunto1], conjuntos[conjunto2]))
+                print("Diferencia:", diferencia(conjuntos[conjunto1], conjuntos[conjunto2])) #Mostrar Diferencia
+
             elif operacion == 5:
                 print("Diferencia Simétrica")
-                conjunto1 = input("Ingrese el primer conjunto a operar: ")
-                conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                while True:#Validar si el Conjunto Existe
+                    conjunto1 = input("Ingrese el primer conjunto a operar: ")
+                    if conjunto1 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
+                while True:#Validar si el Conjunto Existe
+                    conjunto2 = input("Ingrese el segundo conjunto a operar: ")
+                    if conjunto2 in conjuntos:
+                        break
+                    else:
+                        print("Conjunto no existe")
+                        continue
                 print("Conjunto", conjunto1, ":", conjuntos[conjunto1])
                 print("Conjunto", conjunto2, ":", conjuntos[conjunto2])
-                print("Diferencia Simétrica:", diferencia_simetrica(conjuntos[conjunto1], conjuntos[conjunto2]))
-            elif operacion == 6:
+                print("Diferencia Simétrica:", diferencia_simetrica(conjuntos[conjunto1], conjuntos[conjunto2])) #Mostrar Diferencia Simétrica
+            elif operacion == 6: #Cancelar
                 print("Cancelar")
 
                 continue
 
 
-        elif opt == 3:
-            print("Salir")
+        elif opt == 3: #Salir
+            print("Saliendo...")
         else:
             print("Opción no válida")
         
 
-def construir_conjunto(conjunto):
+def construir_conjunto(conjunto): #Construir Conjunto
     print("Construir Nuevo Conjunto")
     Conjunto = []
-    Nombre = input("Ingrese el nombre del conjunto: ")
+    while True: #Validar Nombre si el Nombre no Existe
+        Nombre = input("Ingrese el nombre del conjunto: ")
+        if Nombre in conjunto:
+            print("Conjunto ya existe")
+        else:
+            break
+
     print("Ingrese los elementos del conjunto: ")
-    while True:
+    while True: #Ingresar Elementos
         Elemento = input("Elemento [Enter para salir]: ")
-        if Elemento == "":
+        if Elemento == "": #Salir si presiona Enter
             print("Conjunto", Nombre, ":", Conjunto)
             print("Conjunto Finalizado")
             break
 
-        if Elemento in conjunto:
+        if Elemento in Conjunto: #Validar si el Elemento no Esta Repetido
+            print("Elemento ya existe")
+
+        if Elemento in conjunto["Universo"]: #Validar si el Elemento esta en el Universo
             Conjunto.append(Elemento)
         else:
             print("Elemento Fuera del Universo")
@@ -112,19 +184,19 @@ def construir_conjunto(conjunto):
 
     
 def complemento(universo, conjunto):
-    return [elemento for elemento in universo if elemento not in conjunto]
+    return [elemento for elemento in universo if elemento not in conjunto] #Complemento
 
 def union(conjunto1, conjunto2):
-    return conjunto1 + [elemento for elemento in conjunto2 if elemento not in conjunto1]
+    return conjunto1 + [elemento for elemento in conjunto2 if elemento not in conjunto1] #Unión
 
 def interseccion(conjunto1, conjunto2):
-    return [elemento for elemento in conjunto1 if elemento in conjunto2]
+    return [elemento for elemento in conjunto1 if elemento in conjunto2] #Intersección
 
 def diferencia(conjunto1, conjunto2):
-    return [elemento for elemento in conjunto1 if elemento not in conjunto2]
+    return [elemento for elemento in conjunto1 if elemento not in conjunto2]    #Diferencia 
 
 def diferencia_simetrica(conjunto1, conjunto2):
-    return diferencia(conjunto1, conjunto2) + diferencia(conjunto2, conjunto1)
+    return diferencia(conjunto1, conjunto2) + diferencia(conjunto2, conjunto1) #Diferencia Simétrica
 
 
 main()
